ef73bb27eeaa51a7a9b3f290f212d9968c1e086f
xiaoran1 <wscqkevin@gmail.com>
2018-06-24 14:36:49 +0800

    config maven for UberJar and spring integration

--- src/main/java/indi/kevin/learn/AppMain.java
+++ src/main/java/indi/kevin/learn/AppMain.java
@@ -1,9 +1,14 @@
 package indi.kevin.learn;
 
+import java.util.Optional;
+
 public class AppMain {
 
     public static void main(String[] args){
         System.out.println("first App start");
+        String nullAbleStr = "nullAble String";
+        Optional<String> myStr = Optional.ofNullable(nullAbleStr);
+        System.out.println(myStr.get());
     }
 
 }
